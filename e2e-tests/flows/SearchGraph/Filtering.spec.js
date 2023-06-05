9d16e77563a798ae298115ed5879a9d79bb6affa
orishac <62713593+orishac@users.noreply.github.com>
2023-06-05 22:17:28 +0300

    try clicking on bubble - fail

--- e2e-tests/flows/SearchGraph/Filtering.spec.js
+++ e2e-tests/flows/SearchGraph/Filtering.spec.js
@@ -78,3 +78,69 @@ test('NTIRPs Search check visable', async ({ page }) => {
 
   expect(isBubbleVisible).toBe(true);
 });
+
+test.only('NTIRPs click on bubble', async ({ page }) => {
+  await page.getByRole('button', { name: " Visualize Your Data" }).click()
+  await page.getByText("test1").click()
+  await page.waitForTimeout(3000)
+  await page.getByText("NTIRPs Search").click()
+
+  await page.locator('.col-sm-4').nth(1).locator('th').first().locator("input[type=checkbox]").click({ force: true })
+  await page.locator('.col-sm-4').nth(2).locator('th').first().locator("input[type=checkbox]").click({ force: true })
+  await page.getByRole("button", { name: "Search" }).click()
+
+  const canvas = page.locator('canvas')
+  const canvasBoundingBox = await canvas.boundingBox()
+  const { x, y, width, height } = canvasBoundingBox;
+
+  await page.waitForTimeout(3000)
+
+  const clickStep = 5; // Adjust this value to control the step size of each click
+  const maxRight = x + width;
+  const maxBottom = y + height;
+
+  let currentX = x;
+  let currentY = y;
+  let textShown = false;
+
+  while (currentY <= maxBottom) {
+    while (currentX <= maxRight) {
+      await page.mouse.click(currentX, currentY);
+
+      try {
+        // Check if the text "Selected TIRP info" is visible
+        const textElement = await page.waitForSelector('text=Selected TIRP info', { timeout: 500 });
+
+        if (textElement) {
+          const textContent = await textElement.textContent();
+          if (textContent.includes('Selected TIRP info')) {
+            textShown = true;
+            // Do any additional operations you need when the text is shown
+            break;
+          } else if (textContent.includes('NTIRP TABLE')) {
+            // Stop the loop if "NTIRP TABLE" is shown
+            return;
+          }
+        }
+      } catch (error) {
+      }
+
+      currentX += clickStep;
+    }
+
+    if (textShown) {
+      break;
+    }
+
+    currentX = maxRight;
+    currentY += clickStep;
+    await page.mouse.click(currentX, currentY);
+
+    if (currentY <= maxBottom) {
+      currentX -= clickStep;
+    }
+  }
+
+  await page.getByRole('button', { name: "Explore TIRP" }).click()
+  await page.waitForTimeout(3000)
+})
