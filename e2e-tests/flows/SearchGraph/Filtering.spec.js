8e3dadf86ec59fb9ed1f9505e5ba0fc2fd48393d
kfirs127 <62557231+kfirs127@users.noreply.github.com>
2023-06-05 19:28:25 +0300

    added test to check the visualization of the bubble graph

--- e2e-tests/flows/SearchGraph/Filtering.spec.js
+++ e2e-tests/flows/SearchGraph/Filtering.spec.js
@@ -6,7 +6,6 @@ test.beforeEach(async ({ page }) => {
 });
 
 test('NTIRPs Search filtering', async ({ page }) => {
-    // Expect a title "to contain" a substring.
     await page.getByRole('button', { name: " Visualize Your Data" }).click()
     await page.getByText("test1").click()
     await page.waitForTimeout(3000)
@@ -17,24 +16,65 @@ test('NTIRPs Search filtering', async ({ page }) => {
     await page.waitForTimeout(3000)
 });
 
-test.only('NTIRPs Search filtering by params', async ({ page }) => {
-  // Expect a title "to contain" a substring.
+test('NTIRPs Search filtering by V.S and M.H.S', async ({ page }) => {
   await page.getByRole('button', { name: " Visualize Your Data" }).click()
   await page.getByText("test1").click()
   await page.waitForTimeout(3000)
   await page.getByText("NTIRPs Search").click()
 
-  const inputElementMax = await page.$('.limits-table input[name="maxVSCls0"]');
-  await inputElementMax.fill('70');
-  await inputElementMax.dispatchEvent('input');
+  const inputElementMax_VS = await page.$('.limits-table input[name="maxVSCls0"]')
+  await inputElementMax_VS.fill('80')
+  await inputElementMax_VS.dispatchEvent('input')
+  const inputElementMin_VS = await page.$('.limits-table input[name="minVSCls0"]')
+  await inputElementMin_VS.fill('50')
+  await inputElementMin_VS.dispatchEvent('input')
 
-  const inputElementMin = await page.$('.limits-table input[name="minVSCls0"]');
-  await inputElementMin.fill('50');
-  await inputElementMin.dispatchEvent('input');
+  const inputElementMax_MHS = await page.$('.limits-table input[name="maxHSCls0"]')
+  await inputElementMax_MHS.fill('99')
+  await inputElementMax_MHS.dispatchEvent('input')
+  const inputElementMin_MHS = await page.$('.limits-table input[name="minHSCls0"]')
+  await inputElementMin_MHS.fill('3')
+  await inputElementMin_MHS.dispatchEvent('input')
+
 
   await page.waitForTimeout(3000)
-  // await page.locator('.col-sm-4').nth(1).locator('th').first().locator("input[type=checkbox]").click({ force: true })
-  // await page.locator('.col-sm-4').nth(2).locator('th').first().locator("input[type=checkbox]").click({ force: true })
+
   await page.getByRole("button", { name: "Search" }).click()
   await page.waitForTimeout(3000)
+});
+
+test('NTIRPs Search copmlete filtering', async ({ page }) => {
+  await page.getByRole('button', { name: " Visualize Your Data" }).click()
+  await page.getByText("test1").click()
+  await page.waitForTimeout(3000)
+  await page.getByText("NTIRPs Search").click()
+
+  await page.locator('.col-sm-4').nth(0).locator('th').first().locator("input[type=checkbox]").click({ force: true })
+  await page.locator('.col-sm-4').nth(2).locator('th').first().locator("input[type=checkbox]").click({ force: true })
+  await page.waitForTimeout(3000)
+
+  const inputElementMin_VS = await page.$('.limits-table input[name="minVSCls0"]')
+  await inputElementMin_VS.fill('50')
+  await inputElementMin_VS.dispatchEvent('input')
+
+  await page.waitForTimeout(3000)
+
+  await page.getByRole("button", { name: "Search" }).click()
+  await page.waitForTimeout(3000)
+});
+
+test('NTIRPs Search check visable', async ({ page }) => {
+  await page.getByRole('button', { name: " Visualize Your Data" }).click()
+  await page.getByText("test1").click()
+  await page.waitForTimeout(3000)
+  await page.getByText("NTIRPs Search").click()
+
+  await page.waitForSelector('[test_id="bubble graph"]');
+  const isBubbleVisible = await page.evaluate(() => {
+    const bubble = document.querySelector('[test_id="bubble graph"]');
+    const isVisible = bubble.offsetHeight > 0 && bubble.offsetWidth > 0;
+    return isVisible
+  });
+
+  expect(isBubbleVisible).toBe(true);
 });
--- src/Components/Content/Visualization/Negatives/NSearchGraph.js
+++ src/Components/Content/Visualization/Negatives/NSearchGraph.js
@@ -258,8 +258,8 @@ class NSearchGraph extends Component {
 		return (
 			<div>
 				<Row>
-					<Col>
-						<Bubble
+					<Col >
+						<Bubble test_id="bubble graph"
 							height={100}
 							options={{
 								onClick: (e) => {
